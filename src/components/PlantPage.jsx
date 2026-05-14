21ba40fbabc5455dcf16b7ac6aa7bcab1c2bbfda
Elvis Chege <elvischege98@gmail.com>
2026-05-14 10:17:29 +0300

    Finished coding

--- src/components/PlantPage.jsx
+++ src/components/PlantPage.jsx
@@ -1,14 +1,21 @@
-import React from "react";
+import React ,{useState}from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
-function PlantPage() {
+function PlantPage({plants,setPlants}) {
+const [searchQuery, setSearchQuery] = useState("");
+
+const filteredPlants = plants.filter((plant) =>
+  plant.name.toLowerCase().includes(searchQuery.toLowerCase())
+);
+
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm setPlants={setPlants} />
+      <Search searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
+      <PlantList plants={filteredPlants} setPlants={setPlants} />
     </main>
   );
 }
